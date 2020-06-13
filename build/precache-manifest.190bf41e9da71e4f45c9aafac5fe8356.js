c2fb0d58eb017e40e0df00f0e9e9b629d2572bfe
Levi Plant <levijplant@gmail.com>
2020-06-13 12:06:31 -0700

    project 16 completed

---
+++ build/precache-manifest.190bf41e9da71e4f45c9aafac5fe8356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb0cd24f3892da55a44d29bbd61f6c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7845e2a6992abfd10072",
+    "url": "/static/css/main.61d82ac5.chunk.css"
+  },
+  {
+    "revision": "057822a738ef70c344fc",
+    "url": "/static/js/2.3daa1c74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3daa1c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7845e2a6992abfd10072",
+    "url": "/static/js/main.b47be009.chunk.js"
+  },
+  {
+    "revision": "16385764b937c4287c41",
+    "url": "/static/js/runtime-main.0f4090cb.js"
+  }
+]);
